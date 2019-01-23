3bab2b229e9fdece58693e052fae445a6ed90753
Patryk Ciepiela <machina123@boun.cr>
2019-01-23 22:12:16 +0100

    Project completion

--- assignment/store_filter.js
+++ assignment/store_filter.js
@@ -17,21 +17,81 @@ function showFileContents() {
         var nodes = xml.evaluate(path, xml, null, XPathResult.ANY_TYPE, null);
         resultDomElem.innerHTML = "";
         while(result = nodes.iterateNext()) {
-            resultDomElem.innerHTML += "phone/@type = " + result.getAttribute("type") + "<br>";
-            // console.log(result);
-            console.log(result.childNodes.length);
+            resultDomElem.innerHTML += "Type: " + result.getAttribute("type") + "<br>";
             for(var i = 0; i < result.childNodes.length; i++) {
                 child = result.childNodes[i];
-                console.log(typeof(child))
+                if(child.nodeType == 3) continue;
+                resultDomElem.innerHTML += child.nodeName + ": " + child.childNodes[0].nodeValue;
+                if(child.hasAttributes()) {
+                    for(var j=0; j<child.attributes.length; j++) {
+                        resultDomElem.innerHTML += "<em> (" + child.attributes[j].name + ": " + child.attributes[j].value + ")</em>";
+                    }
+                }
+                resultDomElem.innerHTML += "<br>"
             }
+            resultDomElem.innerHTML += "<hr>";
+        }
+    })
+}
+
+function showSortedFileContents(event) {
+    var resultDomElem = document.getElementById("show-all");
+    var type=event.srcElement.getAttribute("data-type");
+    getDocument(function(xhr) {
+        var xml = xhr.responseXML;
+        var path = "//phone[@type='"+type + "']";
+        var nodes = xml.evaluate(path, xml, null, XPathResult.ANY_TYPE, null);
+        resultDomElem.innerHTML = "";
+        while(result = nodes.iterateNext()) {
+            resultDomElem.innerHTML += "Type: " + result.getAttribute("type") + "<br>";
+            for(var i = 0; i < result.childNodes.length; i++) {
+                child = result.childNodes[i];
+                if(child.nodeType == 3) continue;
+                resultDomElem.innerHTML += child.nodeName + ": " + child.childNodes[0].nodeValue;
+                if(child.hasAttributes()) {
+                    for(var j=0; j<child.attributes.length; j++) {
+                        resultDomElem.innerHTML += "<em> (" + child.attributes[j].name + ": " + child.attributes[j].value + ")</em>";
+                    }
+                }
+                resultDomElem.innerHTML += "<br>"
+            }
+            resultDomElem.innerHTML += "<hr>";
+        }
+    })
+}
+
+function showSelectedNode() {
+    var resultDomElem = document.getElementById("show-all");
+    var nodename=document.getElementById("node").value;
+    getDocument(function(xhr) {
+        var xml = xhr.responseXML;
+        var path = "//" + nodename;
+        var nodes = xml.evaluate(path, xml, null, XPathResult.ANY_TYPE, null);
+        resultDomElem.innerHTML = "";
+        while(result = nodes.iterateNext()) {
+            resultDomElem.innerHTML += result.nodeName + ": " + result.childNodes[0].nodeValue;
+            if(result.hasAttributes()) {
+                for(var j=0; j<result.attributes.length; j++) {
+                    resultDomElem.innerHTML += "<em> (" + result.attributes[j].name + ": " + result.attributes[j].value + ")</em>";
+                }
+            }
+            resultDomElem.innerHTML += "<br>"
+            resultDomElem.innerHTML += "<hr>";
         }
     })
 }
 
 function documentReady() {
     var showAllButtonDomElem = document.getElementById("btnshowall");
-
     showAllButtonDomElem.addEventListener("click", showFileContents, false);
+
+    var showSortedButtons = document.querySelectorAll(".show-sorted");
+    showSortedButtons.forEach(function(elem, key, parent){
+        elem.addEventListener("click", showSortedFileContents, false);
+    });
+
+    var showSelectedNodeButton = document.getElementById("shownode");
+    showSelectedNodeButton.addEventListener("click", showSelectedNode, false);
 }
 
 window.addEventListener("load", documentReady, false);
